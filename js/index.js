19373017f5e65fc4ea4d90f025f674dfceca11b5
sayed mohamed <sm6502282@gmail.com>
2024-11-13 21:12:02 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#element", {
+  strings: ["Developer", "Designer", "Larry Daniels"],
+  typeSpeed: 40,
+  backSpeed: 40,
+  startDelay: 30,
+  backDelay: 1000,
+  loop: true,
+  loopCount: Infinity,
+});
+
